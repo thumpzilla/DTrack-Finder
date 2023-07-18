22f38e1b7ca0a37565bafe029a5590d56eb3502a
DonRaz <131517474+DonRaz@users.noreply.github.com>
2023-07-18 17:32:17 +0300

    Added debounce to prevent overloading the DB, Changed DB Structure. (STILL NOT COMPLETE)
    Killing me softlyy....

--- FirebaseManager.js
+++ FirebaseManager.js
@@ -6,7 +6,11 @@ export default class FirebaseManager {
 
     this.db = null;
     this.user = null;
+    this.favoritesToAdd = {};
+    this.favoritesToRemove = {};
     FirebaseManager.instance = this;
+
+    this.syncFavorites = this.debounce(this.syncFavorites.bind(this), 10000);
   }
 
   // Method to get firestore instance
@@ -16,42 +20,103 @@ export default class FirebaseManager {
     }
     return this.db;
   }
+
   setUser(user) {
     this.user = user;
   }
 
-  // Method to add a favorite song
-  async addFavorite(songName, artist) {
-    return await this.getDb()
-      .collection("favorites")
-      .doc(this.user.uid)
-      .set(
-        {
-          [`${songName}-${artist}`]: {
-            songName: songName,
-            artist: artist,
-          },
-        },
-        { merge: true }
-      );
+  // add a song to favoritesToAdd and call the debounced function
+  addFavorite(songName, artist) {
+    const key = `${songName}-${artist}`;
+
+    // if the song is in the remove list, remove it from there
+    if (this.favoritesToRemove[key]) {
+      delete this.favoritesToRemove[key];
+    } else {
+      // otherwise, add it to the add list
+      this.favoritesToAdd[key] = {
+        songName: songName,
+        artist: artist,
+      };
+    }
+
+    this.syncFavorites();
+  }
+
+  // remove a song from the favorites
+  removeFavorite(songName, artist) {
+    const key = `${songName}-${artist}`;
+
+    // if the song is in the add list, remove it from there
+    if (this.favoritesToAdd[key]) {
+      delete this.favoritesToAdd[key];
+    } else {
+      // otherwise, add it to the remove list
+      this.favoritesToRemove[key] = true;
+    }
+
+    this.syncFavorites();
+  }
+
+  // this function will be called at most once every 10 seconds
+  async syncFavorites() {
+    if (this.user) {
+      try {
+        const batch = this.getDb().batch();
+
+        // Add favorites
+        for (let key in this.favoritesToAdd) {
+          const favRef = this.getDb()
+            .collection("users")
+            .doc(this.user.uid)
+            .collection("favorites")
+            .doc(key);
+          batch.set(favRef, this.favoritesToAdd[key]);
+        }
+
+        // Remove favorites
+        for (let key in this.favoritesToRemove) {
+          const favRef = this.getDb()
+            .collection("users")
+            .doc(this.user.uid)
+            .collection("favorites")
+            .doc(key);
+          batch.delete(favRef);
+        }
+
+        await batch.commit();
+
+        this.favoritesToAdd = {};
+        this.favoritesToRemove = {};
+      } catch (error) {
+        console.error("Error syncing favorites: ", error);
+        // handle the error
+      }
+    }
   }
 
   // Method to get all favorite songs
   async getFavorites() {
-    const favoritesSnapshot = await this.getDb()
-      .collection("favorites")
-      .doc(this.user.uid)
-      .get();
-    return favoritesSnapshot.exists ? favoritesSnapshot.data() : {};
+    try {
+      const favoritesSnapshot = await this.getDb()
+        .collection("users")
+        .doc(this.user.uid)
+        .collection("favorites")
+        .get();
+      return favoritesSnapshot.docs.map((doc) => doc.data());
+    } catch (error) {
+      console.error("Error getting favorites: ", error);
+      // handle the error
+    }
   }
 
-  // Method to remove a favorite song
-  async removeFavorite(songName, artist) {
-    return await this.getDb()
-      .collection("favorites")
-      .doc(this.user.uid)
-      .update({
-        [`${songName}-${artist}`]: firebase.firestore.FieldValue.delete(),
-      });
+  // debounce function
+  debounce(func, delay) {
+    let debounceTimer;
+    return function (...args) {
+      const context = this;
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
   }
 }
